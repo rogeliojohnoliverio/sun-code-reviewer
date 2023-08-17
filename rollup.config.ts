0fe9eb3021e4680186e982bbaf6afd58ba4ae5da
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2023-08-17 16:08:21 +0800

    PJ_SIM_CHATGPT_2023-59 [Code Reviewer] Integrate OpenAI code reviewer

---
+++ rollup.config.ts
@@ -0,0 +1,23 @@
+import esbuild from 'rollup-plugin-esbuild';
+import { defineConfig } from 'rollup';
+
+export default defineConfig([
+  {
+    input: 'src/bot.ts',
+    output: {
+      dir: 'dist/lib/',
+      format: 'esm',
+      inlineDynamicImports: false,
+    },
+    plugins: [esbuild({ include: 'src/*.ts' })],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist/',
+      format: 'esm',
+      inlineDynamicImports: false,
+    },
+    plugins: [esbuild()],
+  },
+]);
--- src/chat.ts
+++ src/chat.ts
@@ -1,18 +1,38 @@
+import { ChatGPTAPI } from 'chatgpt';
 import { Inputs } from './input';
 import { Prompts } from './prompt';
 
 export class Chat {
+  private chatAPI: ChatGPTAPI;
+
+  constructor(apikey: string) {
+    this.chatAPI = new ChatGPTAPI({
+      apiKey: apikey,
+      apiBaseUrl: 'https://api.openai.com/v1',
+      completionParams: {
+        model: 'gpt-3.5-turbo',
+        temperature: +(process.env.temperature || 0) || 1,
+        top_p: +(process.env.top_p || 0) || 1,
+        max_tokens: process.env.max_tokens
+          ? +process.env.max_tokens
+          : undefined,
+      },
+    });
+  }
+
   private generatePrompt = (filename: string, patch: string) => {
     const prompts = new Prompts();
     const input = new Inputs(filename, patch);
     return input.render(prompts.reviewFilePrompt);
   };
+
   public codeReview = async (filename: string, patch: string) => {
     if (!patch) {
       return '';
     }
 
     const prompt = this.generatePrompt(filename, patch);
-    return prompt;
+    const res = await this.chatAPI.sendMessage(prompt);
+    return res.text;
   };
 }
